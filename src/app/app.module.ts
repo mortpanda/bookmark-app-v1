ec8e10966dd26338ec2d2cb079c6be4eb56506ab
Kent Nagao <31921593+mortpanda@users.noreply.github.com>
2022-06-03 09:27:02 +0900

    main
    main

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,7 +5,7 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavbarComponent } from './shared/navbar/navbar.component';
 import { LandingComponent } from './landing/landing.component';
-import {DockModule} from 'primeng/dock';
+import { DockModule } from 'primeng/dock';
 import { MenuModule } from 'primeng/menu';
 import { MenubarModule } from 'primeng/menubar';
 import { ToolbarModule } from 'primeng/toolbar';
@@ -17,22 +17,22 @@ import { FlexLayoutModule } from '@angular/flex-layout';
 import { ToolbarComponent } from './shared/toolbar/toolbar.component';
 import { SmallscreenNavComponent } from './shared/smallscreen-nav/smallscreen-nav.component';
 import { StartComponent } from './start/start.component';
-import {CardModule} from 'primeng/card';
+import { CardModule } from 'primeng/card';
 import { BookmarksComponent } from './bookmarks/bookmarks.component';
-import {MessagesModule} from 'primeng/messages';
-import {MessageModule} from 'primeng/message';
-import {ToastModule} from 'primeng/toast';
-import {MessageService} from 'primeng/api';
-import {TableModule} from 'primeng/table';
-import {ProgressSpinnerModule} from 'primeng/progressspinner';
+import { MessagesModule } from 'primeng/messages';
+import { MessageModule } from 'primeng/message';
+import { ToastModule } from 'primeng/toast';
+import { MessageService } from 'primeng/api';
+import { TableModule } from 'primeng/table';
+import { ProgressSpinnerModule } from 'primeng/progressspinner';
 import { SaveBookmarkComponent } from './shared/save-bookmark/save-bookmark.component';
-import { MatDialogModule } from '@angular/material/dialog'; 
-import {InputTextModule} from 'primeng/inputtext';
-import {InputTextareaModule} from 'primeng/inputtextarea';
-import { FormsModule }   from '@angular/forms';
-import {ProgressBarModule} from 'primeng/progressbar';
-import {DropdownModule} from 'primeng/dropdown';
-import {DividerModule} from 'primeng/divider';
+import { MatDialogModule } from '@angular/material/dialog';
+import { InputTextModule } from 'primeng/inputtext';
+import { InputTextareaModule } from 'primeng/inputtextarea';
+import { FormsModule } from '@angular/forms';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { DividerModule } from 'primeng/divider';
 
 
 @NgModule({
@@ -45,8 +45,8 @@ import {DividerModule} from 'primeng/divider';
     StartComponent,
     BookmarksComponent,
     SaveBookmarkComponent,
-    
-    
+
+
 
   ],
   imports: [
@@ -75,9 +75,9 @@ import {DividerModule} from 'primeng/divider';
     ProgressBarModule,
     DropdownModule,
     DividerModule,
-    
+
   ],
-  providers: [MessageService,SaveBookmarkComponent ],
+  providers: [MessageService, SaveBookmarkComponent],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
